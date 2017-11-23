d92a8138898813ae5503b93c2243af2fe8d0195a
AlkalineCandy <agnes.adel@gmail.com>
2017-11-23 22:59:04 +0100

    lotsa stuff

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-/* Slider */
+
+// Click function for the toggles
+function main() {}
+$('.box2').hide();
+  $(".box1").on("click", function(){
+    $(this).children('.box2').toggle();
+    $(this).toggleClass("active");
+  });
+
+$(document).ready(main);
+
+
+// Slider
 
 var slideIndex = 1;
 showDivs(slideIndex);
@@ -17,61 +29,5 @@ function showDivs(n) {
     }
     x[slideIndex-1].style.display = "block";
 }
-
-/* Flexbox */
-
-
-function main() {}
-$('.box10').hide();
-  $(".box1").on("click", function(){
-    $(".box10").toggle();
-    $(this).toggleClass("active");
-  });
-
-$(document).ready(main);
-
-function main() {}
-$('.box20').hide();
-  $(".box2").on("click", function(){
-    $(".box20").toggle();
-    $(this).toggleClass("active");
-  });
-
-$(document).ready(main);
-
-function main() {}
-$('.box30').hide();
-  $(".box3").on("click", function(){
-    $(".box30").toggle();
-    $(this).toggleClass("active");
-  });
-
-$(document).ready(main);
-
-
-function main() {}
-$('.box40').hide();
-  $(".box4").on("click", function(){
-    $(".box40").toggle();
-    $(this).toggleClass("active");
-  });
-
-$(document).ready(main);
-
-function main() {}
-$('.box50').hide();
-  $(".box5").on("click", function(){
-    $(".box50").toggle();
-    $(this).toggleClass("active");
-  });
-
-$(document).ready(main);
-
-function main() {}
-$('.box60').hide();
-  $(".box6").on("click", function(){
-    $(".box60").toggle();
-    $(this).toggleClass("active");
-  });
 
 $(document).ready(main);
